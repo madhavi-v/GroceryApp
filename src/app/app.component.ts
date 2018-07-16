609507580bf94f337eafb004f0ab89cd33ea29eb
madhavi-v <38600169+madhavi-v@users.noreply.github.com>
2018-07-15 22:11:04 -0700

    Grocery App commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,36 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormControl } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'app';
+export class AppComponent implements OnInit {
+  
+ 
+  groceryList : FormGroup;
+  displayList1 : Array<any> = [];
+  
+groceryItems : Array<any>;
+  ngOnInit(){
+    this.groceryList = new FormGroup({
+      listItem : new FormControl('')
+    })
+    
+  }
+  getGroceryList(groceryList : FormGroup){
+     
+    this.displayList1.push(groceryList.value.listItem) ;
+    this.groceryList.reset();
+    
+  }
+
+  clearDiv(){
+    
+    this.displayList1 = [];
+  }
+ 
+  
+
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,26 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { MatFormFieldModule, MatInputModule, MatCardModule, MatListModule} from '@angular/material';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { FlexLayoutModule } from '@angular/flex-layout';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule,
+    MatFormFieldModule,
+    MatInputModule,
+    BrowserAnimationsModule,
+    ReactiveFormsModule,
+    MatCardModule,
+    FlexLayoutModule,
+    MatListModule,
+    MatCardModule
   ],
   providers: [],
   bootstrap: [AppComponent]
